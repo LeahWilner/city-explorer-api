9043665808494099217852bf184e3698f9bc1111
Leah Wilner <leahwilner1@gmail.com>
2023-01-28 16:12:00 -0600

    returning weather object

--- server.js
+++ server.js
@@ -1,50 +1,53 @@
-"use strict";
+'use strict';
 
-const express = require("espress");
-require("dotenv").config();
-let data = require("./data/weather.json");
-const cors = require("cors");
+const express = require('express');
+require('dotenv').config();
+let data = require('./data/weather.json');
+const cors = require('cors');
 
 const app = express();
-app.use(cors);
+app.use(cors());
 const PORT = process.env.PORT || 5005;
 
-app.get("/", (request, response) => {
-  response.send("Hello from server HOME route / !!");
+app.get('/', (request, response) => {
+  response.send('Hello from server HOME route / !!');
 });
 
-app.get("/hello", (request, response) => {
-  let firstName = request.query.name;
-  let lastName = request.query.lastName;
-  response.status(200).send(`Hello, ${firstName} ${lastName}`);
+
+
+app.get('/weather', (request, response) => {
+//   try {
+
+  //localhost:3002/weather?searchquery=Seattle
+  let searchQuery = request.query.searchquery;
+
+  let dataToInstantiate = data.find(weather => weather.city_name === searchQuery);
+  let dataToSend = new Weather(dataToInstantiate);
+
+  response.status(200).send(dataToSend);
+
+//   } catch (error) {
+//     // eslint-disable-next-line no-undef
+//     next(error);
+//   }
 });
 
-app.get("/weather", (request, response) => {
-  try {
-    let weatherForecast = request.query.weatherForeacast;
-    let dataToInstantiate = data.find(
-      (weather) => weather.weatherForecast === weatherForecast
-    );
-    let dataToSend = new Weather(dataToInstantiate);
-    response.status(200).send.dataToSend;
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get("*", (request, response) => {
-  response.status(404).send("The route was not found. Error 404");
+app.get('*', (request, response) => {
+  response.status(404).send('The route was not found. Error 404');
 });
 
 class Weather {
   constructor(weatherObject) {
-    this.weatherForecast = weatherObject.weatherForecast;
-    this.location = weatherObject.location;
+    console.log('CCCCC',weatherObject.data[0].datetime);
+    this.location = weatherObject.city_name;
+    // this.weatherForecast = weatherObject.weather.description;
+    // this.datetime = weatherObject.datetime;
   }
 }
 
-app.use((error, request, response, next) => {
-  response.status(500).send(error.message);
-});
+// eslint-disable-next-line no-unused-vars
+// app.use((error, request, response, next) => {
+//   response.status(500).send(error.message);
+// });
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
