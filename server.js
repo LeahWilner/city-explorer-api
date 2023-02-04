0a5c7dbd7c7889fd7f0183244d23d8c0b069aa78
Leah Wilner <leahwilner1@gmail.com>
2023-02-04 16:39:14 -0600

    getting movie data

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const PORT = process.env.PORT || 5005;
 
 const axios = require('axios');
 
+// let Movies = process.env.MOVIE_API_KEY
+
 app.get('/', (request, response) => {
   response.send('Hello from server HOME route / !!');
 });
 
-
-
 app.get('/weather', (request, response) => {
-//   try {
+  //   try {
 
   //localhost:3002/weather?searchquery=Seattle
   let searchQueryCity = request.query.searchquery;
@@ -26,53 +26,50 @@ app.get('/weather', (request, response) => {
   let lon = request.query.lon;
   console.log(`----------------${lat} + ${lon}`);
 
-  let dataToInstantiate = data.find(weather => weather.city_name.toLowerCase() === searchQueryCity.toLowerCase());
+  let dataToInstantiate = data.find(
+    (weather) =>
+      weather.city_name.toLowerCase() === searchQueryCity.toLowerCase()
+  );
 
   // console.log('dataToInstantiate:',dataToInstantiate);
-  let dataToSend = dataToInstantiate.data.map((weather) => new Weather(weather));
-  // console.log('dataToSend:', dataToSend);
+  let dataToSend = dataToInstantiate.data.map(
+    (weather) => new Weather(weather)
+  );
 
   response.status(200).send(dataToSend);
-
-//   } catch (error) {
-//     // eslint-disable-next-line no-undef
-//     next(error);
-//   }
 });
 
-app.get('*', (request, response) => {
-  response.status(404).send('The route was not found. Error 404');
-});
-
-app.get('/movie', async (request, response, next) => {
+app.get('/movies', async (request, response, next) => {
   try {
-    let frontEndSearchQuery = request.query.searchquery;
-    let url = `https://api.themoviedb.org/3/movie/550?client_id=${process.env.MOVIE_API_KEY}&query=${frontEndSearchQuery}&format=json`;
+    let movieSearchQuery = request.query.searchquery;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movieSearchQuery}&format=json`;
+  
+
     let results = await axios.get(url);
-    let constructorData= results.data;
+    let constructorData = results.data;
+    // reults.map(movie => new(movie, constructorData))
     response.status(200).send(constructorData);
     console.log(results.data);
   } catch (error) {
     next(error);
   }
-
 });
 
-app.get('/restaurant', async (request, response, next) => {
-  try {
-    restaurantSearchQuery = request.query.searchquery;
-    let url = `https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972?client_id=${process.env.YELP_API_KEY}&query=${restaurantSearchQuery}&format=json`;
-    let restaurantResults = await axios.get(url);
-    let restaurantConstructorData = restaurantResults.data;
-    response.status(200).send(restaurantConstructorData);
-  } catch (error) {
-    next(error);
-  }
-});
+// app.get('/restaurant', async (request, response, next) => {
+//   try {
+//     let restaurantSearchQuery = request.query.searchquery;
+//     let url = `https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972?client_id=${process.env.YELP_API_KEY}&query=${restaurantSearchQuery}&format=json`;
+//     let restaurantResults = await axios.get(url);
+//     let restaurantConstructorData = restaurantResults.data;
+//     response.status(200).send(restaurantConstructorData);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 class Weather {
   constructor(weatherObject) {
-    console.log('yo',weatherObject);
+    console.log('yo', weatherObject);
     this.weatherForecast = weatherObject.weather.description;
     this.datetime = weatherObject.datetime;
   }
@@ -81,21 +78,25 @@ class Weather {
 class Movies {
   constructor(movieObject) {
     console.log('yo', movieObject);
-    this.movieObject = movieObject.description;
+    this.movieObject = movieObject.data;
     // this.datetime = weatherObject.datetime;
   }
 }
 
-class Restaurant { 
-  constructor(restaurantObject) {
-    console.log('restaurants?');
-    this.restaurantObject = restaurantObject;
-  }
-}
+// class Restaurant {
+//   constructor(restaurantObject) {
+//     console.log('restaurants?');
+//     this.restaurantObject = restaurantObject;
+//   }
+// }
 
 // eslint-disable-next-line no-unused-vars
 // app.use((error, request, response, next) => {
 //   response.status(500).send(error.message);
 // });
+
+app.get('*', (request, response) => {
+  response.status(404).send('The route was not found. Error 404');
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
